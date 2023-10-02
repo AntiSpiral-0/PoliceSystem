c5c10e54b154b6fefa4cf2d8bdd473260ff6b46a
Yassine Thayer <yassine.ben.thayer.prv@gmail.com>
2023-10-02 15:04:27 +0200

    this is the best editor stupid commit messqge

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 public class Log
 {
     public string Destination { get; set; }
@@ -19,26 +19,50 @@ public class TravelLog
 }
 class Program
 {
-    bool isRunning = true;
-    public void Main(string[] args)
+    static bool isRunning = true;
+    static void Main(string[] args)
     {
         TravelLog travelLog = new TravelLog();
+        while (isRunning)
+        {
         Console.WriteLine("=========================================================");
         Console.WriteLine("Welcome to the travel log");
+        Console.WriteLine("Would you like to see your travel log ? press [s]");
         Console.WriteLine("Would you like to record a visit ? press[r]");
         Console.WriteLine("Would you like to remove a place ? press[re]");
         Console.WriteLine("Would you like to exit ? press[q]");
         string choice = Console.ReadLine();
-        while (isRunning)
-        {
             switch (choice.ToUpper())
             {
                 case "R" :
                     string place = Console.ReadLine() ;
-                    travelLog.Destinations(new Log(place));
+                    travelLog.Destinations.Add(new Log(place));
+                    break;
+                case "RE":
+                    Console.WriteLine("Please choose the destination you want to remove") ;
+                    string choix = Console.ReadLine();
+                    Log ChosenDestination = travelLog.Destinations.FirstOrDefault(Log => Log.Destination == choix);
+                    if(ChosenDestination != null)
+                    {
+                        travelLog.Destinations.Remove(ChosenDestination);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Destination not found");
+                    }
+                    break;
+                case "S" :
+                    Console.WriteLine("This is your travel log");
+                    foreach (Log destination in travelLog.Destinations)
+                    {
+                        Console.WriteLine(destination.Destination);
+                    }
+                    break;
+                case "Q":
+                    Console.WriteLine("Exiting...");
+                    isRunning = false;
+                    break;
             }
-
         }
-
     }
 }
